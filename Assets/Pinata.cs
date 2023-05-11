b23d82e7c90832f7d429a97956bac69691a6e5b7
Elad <cagan@innplaylabs.com>
2023-05-11 21:17:16 +0300

    Added background and more cracks

--- Assets/Pinata.cs
+++ Assets/Pinata.cs
@@ -9,8 +9,24 @@ public class Pinata : MonoBehaviour
     private Rigidbody _pinataRB;
     [SerializeField]
     private float _hitMultiplayer;
+    [SerializeField]
+    private GameObject _hole1;
+    [SerializeField]
+    private GameObject _hole2;
+    [SerializeField]
+    private GameObject _hole3;
+    [SerializeField]
+    private List<AudioClip> _hitSfx;
+    [SerializeField]
+    private List<AudioClip> _pinataSfx;
+    [SerializeField]
+    private AudioClip _intro;
+    [SerializeField]
+    private AudioSource _source;
 
 
+
+    private int _hitCount;
     // Start is called before the first frame update
     void Start()
     {
@@ -51,6 +67,18 @@ public class Pinata : MonoBehaviour
         {
             _pinataRB.AddForce(Vector3.left * _hitMultiplayer);
         }
-
+        _hitCount++;
+        if(_hitCount > 5)
+        {
+            _hole1.SetActive(true);
+        }
+        if (_hitCount > 10)
+        {
+            _hole2.SetActive(true);
+        }
+        if (_hitCount > 15)
+        {
+            _hole3.SetActive(true);
+        }
     }
 }
