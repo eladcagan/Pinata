d8dac12f4874c5dbd3527e6322c08176c8cfcf7f
Elad <eladcagan@gmail.com>
2023-05-16 14:57:02 +0300

    Added Menu + menu logic

---
+++ Assets/MenuManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public void OnSceneSelected(int pinataType)
+    {
+        LoadPinataScene(pinataType);
+    }
+
+    private void LoadPinataScene(int pinataType)
+    {
+        SceneManager.LoadSceneAsync(pinataType);
+    }
+}
